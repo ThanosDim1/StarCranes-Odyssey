6a0b4e5e0b778499216b754336346918013d66a4
Thanasis Dimitrakopoulos <git@github.com>
2024-01-15 13:56:37 +0200

    Fixed enemy collision and offset

--- GameProject/enemy.cpp
+++ GameProject/enemy.cpp
@@ -9,13 +9,14 @@ void Enemy::update(float dt)
 {
 	float delta_time = dt / 1000.0f;
 
+	std::printf("%f" , m_pos_y);
 	// Move left at a constant speed
 	m_pos_x -= (m_max_velocity * delta_time);
+	
 
-
-	// Simulate gravity
-	//m_vy += m_gravity * delta_time;
-	//m_pos_y += m_vy * delta_time;
+	//Stimulate gravity
+	m_vy += m_gravity * delta_time;
+	m_pos_y += m_vy * delta_time;
 
 	GameObject::update(dt);
 
@@ -24,9 +25,10 @@ void Enemy::update(float dt)
 void Enemy::draw()
 {
 	int spritesenemy1 = (int)fmod(100.0f - m_pos_x * 3.0f, m_spritesenemy1.size());
-
+	float offset_x = m_state->m_global_offset_x;
+	float offset_y = m_state->m_global_offset_y;
 	m_brush_enemy.texture = m_spritesenemy1[spritesenemy1];
-	graphics::drawRect(m_pos_x, m_pos_y, 1.0f, 1.0f, m_brush_enemy);
+	graphics::drawRect(m_pos_x + offset_x, m_pos_y + offset_y, 1.0f, 1.0f, m_brush_enemy);
 
 
 	if (m_state->m_debugging)
@@ -36,11 +38,9 @@ void Enemy::draw()
 void Enemy::init()
 {
 	// stage 1
-	m_pos_x = 10.0f;
+	m_pos_x = 5.0f;
 	m_pos_y = 5.0f;
 
-	m_state->m_global_offset_x = m_state->getCanvasWidth() / 2.0f - m_pos_x;
-	m_state->m_global_offset_y = m_state->getCanvasHeight() / 2.0f - m_pos_y;
 
 	m_brush_enemy.fill_opacity = 1.0f;
 	m_brush_enemy.outline_opacity = 0.0f;
